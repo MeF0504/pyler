ffd59d274d1528320cd9cfd4bb4f6019f672966d
Takuro Fujino <fujino.takuro.92@gmail.com>
2023-10-11 21:16:03 +0900

    WIP
    create the default info module and create a process to use the module.

---
+++ lib/pyler_default.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+from datetime import datetime
+
+
+def time_format(time: float):
+    return datetime.fromtimestamp(time).strftime('%Y-%m-%d %H:%M:%S')
+
+
+def get_size(size_byte: int):
+    if size_byte >= 1024**3:
+        size = float(size_byte)/1024**3
+        pre = 'G'
+    elif size_byte >= 1024**2:
+        size = float(size_byte)/1024**2
+        pre = 'M'
+    elif size_byte >= 1024:
+        size = float(size_byte)/1024
+        pre = 'k'
+    else:
+        size = size_byte
+        pre = ''
+    return f'{size:.1f} {pre}B'
+
+
+def main(item: Path):
+    stat = item.stat()
+    ret = '''
+created: {}
+last update: {}
+file size: {}'''.format(time_format(stat.st_birthtime),
+                        time_format(stat.st_mtime),
+                        get_size(stat.st_size))
+    return ret
